5bd06e5168c8750517554d4d81636ff5ecf7b4ee
Yuan Xu <yuanxu@umich.edu>
2022-11-02 16:06:55 -0400

    player movement but with jump bug

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,21 +10,40 @@ public class PlayerControl : MonoBehaviour
     public UnityEvent OnMine = new UnityEvent();
     public UnityEvent OnSwapItem = new UnityEvent();
     public UnityEvent OnChangeForm = new UnityEvent();
+    public float speed = 5;
 
     private Camera playerCam;
     private bool isBirb = false;
+    private Rigidbody2D rb2d;
+    // private CircleCollider2D collider;
+    // private float distToGround;
+    // private bool  is_grounded = false;
+    private bool IsGrounded
+    {
+        get{
+            Debug.Log(new Vector2(rb2d.transform.position.x, rb2d.transform.position.y));
+            Debug.Log(GetComponent<CircleCollider2D>().bounds.extents.y);
+            return Physics2D.Raycast(new Vector2(rb2d.transform.position.x, rb2d.transform.position.y), Vector2.down, GetComponent<CircleCollider2D>().bounds.extents.y + 0.1f);
+        }
+    }
     // Update is called once per frame
     void Start()
     {
         playerCam = Camera.main;
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        Debug.Log("start");
+        // collider = gameObject.GetComponent<CircleCollider2D>();        
     }
 
     void Update()
     {
         // get movement vectors: horizontal, vertical
+        // distToGround = collider.bounds.extents.y;
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector2 moveVec = new Vector2(x, y);
+        rb2d.velocity = new Vector2(x * speed, rb2d.velocity.y);
+        // Debug.Log(x);
         // call OnMoveHullEvent and invoke it passing the movement Vector
         OnMovePlayerCharacter?.Invoke(moveVec.normalized);
 
@@ -61,5 +80,36 @@ public class PlayerControl : MonoBehaviour
             EventBus.Publish(new EventStopTank());
             OnChangeForm?.Invoke();
         }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded) {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 10f);
+        }
+        // Debug.Log(distToGround);
+        // Debug.Log(IsGrounded);
+        // Debug.Log(is_grounded);
+        // OnCollisionStay();
     }
+    
+
+    // private void OnCollisionStay(Collision collisionInfo)
+    // {
+    //     Debug.Log(collisionInfo.gameObject.tag);
+    //     Debug.Log("collision");
+    //     if (collisionInfo.gameObject.CompareTag("Ground")) {
+    //         is_grounded = true;
+    //     }
+    //     else {
+    //         is_grounded = false;
+    //     }
+    // }
+    // private void OnCollisionEnter(Collision collisionInfo)
+    // {
+    //     Debug.Log(collisionInfo.gameObject.tag);
+    //     Debug.Log("collisionenter");
+    //     if (collisionInfo.gameObject.CompareTag("Ground")) {
+    //         is_grounded = true;
+    //     }
+    //     else {
+    //         is_grounded = false;
+    //     }
+    // }
 }
