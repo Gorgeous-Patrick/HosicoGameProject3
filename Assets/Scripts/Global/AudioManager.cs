6414e3dded7e9f2ef4eeaa87dedce92557133540
Patrick Li <1543162420@qq.com>
2022-11-06 00:14:35 -0400

    feat: audio manager ready

---
+++ Assets/Scripts/Global/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class AudioManager : MonoBehaviour
+{
+
+  public static AudioManager instance;
+  Dictionary<string, AudioClip> audioSources;
+  [SerializeField] List<AudioClip> audioClips;
+  [SerializeField] List<string> audioNames;
+
+  void Awake()
+  {
+    if (instance == null)
+    {
+      instance = this;
+    }
+    else
+    {
+      Destroy(gameObject);
+      return;
+    }
+  }
+
+  void Start()
+  {
+    audioSources = new Dictionary<string, AudioClip>();
+    if (audioClips.Count != audioNames.Count)
+    {
+      Debug.LogError("AudioManager: audioClips and audioNames are not the same size");
+    }
+    for (int i = 0; i < audioClips.Count; i++)
+    {
+      audioSources.Add(audioNames[i], audioClips[i]);
+    }
+  }
+
+  public void playSound(string name, float volume = 1.0f)
+  {
+    AudioSource.PlayClipAtPoint(audioSources[name], Camera.main.transform.position, volume);
+    Debug.Log("play sound" + name);
+  }
+
+}
