198f7c635d709420bf9835fc52ada9aec15b937f
Zongxi Huang <zongxih@umich.edu>
2022-11-07 02:40:43 -0500

    Added two more sections to linear level

--- Assets/Scripts/InteractiveObjects/Generator.cs
+++ Assets/Scripts/InteractiveObjects/Generator.cs
@@ -9,11 +9,14 @@ public class Generator : Interactive
   {
     EventBus.Publish(new EventBatteryStatusChange { charging = true });
     EventBus.Publish(new EventChangeCheckpoint {checkpoint = transform});
+        PlayerPrefs.SetFloat("x-pos", transform.position.x);
+        PlayerPrefs.SetFloat("y-pos", transform.position.y);
+        PlayerPrefs.SetFloat("z-pos", transform.position.z);
+        Debug.Log("Player random prefs" + PlayerPrefs.GetFloat("z-pos"));
   }
 
   protected override void Interrupt()
   {
     EventBus.Publish(new EventBatteryStatusChange { charging = false });
   }
-
 }
