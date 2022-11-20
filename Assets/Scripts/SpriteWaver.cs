4b6a7b8d6bef53bae85071dc4f366330f7a632c1
Salty Fish <saltyfish@sjtu.edu.cn>
2022-11-20 13:45:46 -0500

    chore: cleanup & re-arrange scripts

---
+++ Assets/Scripts/SpriteWaver.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteWaver : MonoBehaviour
+{
+  [SerializeField] float speed = 5f;
+  [SerializeField] float height = 0.5f;
+
+  void Update()
+  {
+    Vector2 pos = icon.position;
+    float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+    //set the object's Y to the new calculated Y
+    transform.position = new Vector3(pos.x, newY, 1);
+  }
+}
