d14c2b96895ff1e53b5dd5054154cf18ee8e1302
Salty Fish <saltyfish@sjtu.edu.cn>
2022-11-19 21:10:48 -0500

    WIP: climbing on soft ropes

--- Assets/Scripts/InteractiveObjects/Climbable.cs
+++ Assets/Scripts/InteractiveObjects/Climbable.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ClimbStatus {Ladder, Rope, None}
+
 public class Climbable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public ClimbStatus type;
 }
