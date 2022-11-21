63ffe03ef59dd033cdb49339371ba8e463980c0f
henrlin24 <102765166+henrlin24@users.noreply.github.com>
2022-11-21 05:48:45 -0500

    Added rock collapse SFX, used in title and tutorial scene atm

--- Assets/Scripts/Logistics/LoadsSceneOnClick.cs
+++ Assets/Scripts/Logistics/LoadsSceneOnClick.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 
 public class LoadsSceneOnClick : MonoBehaviour
 {
-    [SerializeField] bool isClicked = false;
+    
 
     async void startAnalytics()
     {
@@ -25,8 +25,14 @@ public class LoadsSceneOnClick : MonoBehaviour
         Debug.Log("Analytics Start Error");
         }
     }
+
     [SerializeField] string SceneToLoad = "Level 1";
     [SerializeField] GameObject UserConsent;
+    [SerializeField] bool isClicked = false;
+    AudioSource clickSFX;
+    private void Start() {
+        clickSFX = GetComponent<AudioSource>();
+    }
 
     public void LoadScene() {
         if (isClicked) { return;  };
@@ -34,9 +40,20 @@ public class LoadsSceneOnClick : MonoBehaviour
     }
 
     IEnumerator ClickSequence() {
+        // prevent spamming
         isClicked = true;
+
+        // start audio clip 
+        if (clickSFX != null) {
+            clickSFX.Play();
+        }
+
+        // call for transition
         EventBus.Publish<EventStartTransition>(new EventStartTransition{ isStart = true });
         yield return new WaitForSeconds(1.75f);
+
+        // additional time to allow audio to finish playing - currently for title screen
+        yield return new WaitForSeconds(2.25f);
 
         if (SceneManager.GetActiveScene().name != "Title")
             SceneToLoad = PlayerPrefs.GetString("currScene");
