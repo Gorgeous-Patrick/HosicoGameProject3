8d4525d52192dbd733daf122887c7b942638c07f
henrlin24 <102765166+henrlin24@users.noreply.github.com>
2022-12-04 15:27:33 -0500

    Pushable crate now destroyable

--- Assets/Scripts/InteractiveObjects/IsBreakable.cs
+++ Assets/Scripts/InteractiveObjects/IsBreakable.cs
@@ -5,50 +5,51 @@ using UnityEngine;
 public class IsBreakable : MonoBehaviour
 {
 
-  [SerializeField] int durability = 3; // takew how many hits to be destroyed
-  [SerializeField] bool canHit = true;
-  float toughness = 0.3f; // larger: takes more time to mine
-  [SerializeField] bool onlyDynamite = false;
+    [SerializeField] int durability = 3; // takew how many hits to be destroyed
+    [SerializeField] bool canHit = true;
+    float toughness = 0.3f; // larger: takes more time to mine
+    [SerializeField] bool onlyDynamite = false;
+    [SerializeField] GameObject spawnObject;
 
-  // indicates the time remaining until next durability decrement
-  [SerializeField] float countdown;
+    // indicates the time remaining until next durability decrement
+    [SerializeField] float countdown;
 
-  void Start()
-  {
-    countdown = toughness;
-  }
-
-  void alterDurability(int delta)
-  {
-    if (!canHit) return;
-    durability += delta;
-    // plays animation
-    // ParticleSystemManager.RequestParticlesAtPositionAndDirection(transform.position, Vector3.up);
-    if (GetComponent<SpriteFlasher>() != null)
-    {
-      GetComponent<SpriteFlasher>().FlashFunc();
+    void Start() {
+        countdown = toughness;
     }
-    if (durability <= 0)
-    {
-      if (!transform.CompareTag("BreakableGround"))
-      {
-        AudioManager.instance.playSound("3-dig_rocks", 1.0f);
-        Destroy(gameObject);
-      }
 
-    }
-  }
+    void alterDurability(int delta) {
+        if (!canHit) return;
+        durability += delta;
+        // plays animation
+        // ParticleSystemManager.RequestParticlesAtPositionAndDirection(transform.position, Vector3.up);
+        if (GetComponent<SpriteFlasher>() != null) {
+            GetComponent<SpriteFlasher>().FlashFunc();
+        }
+        if (durability <= 0) {
+            if (!transform.CompareTag("BreakableGround")) {
+                AudioManager.instance.playSound("3-dig_rocks", 1.0f);
 
-  void OnTriggerStay2D(Collider2D collisionInfo)
-  {
-    DestroysBreakables breaker = collisionInfo.gameObject.GetComponent<DestroysBreakables>();
-    if (breaker == null || (onlyDynamite && !breaker.isExplosion)) return;
-    countdown -= Time.deltaTime;
-    if (countdown <= 0 || breaker.isExplosion)
-    {
-      countdown = toughness;
-      alterDurability(breaker.durabilityImpact);
+                // spawns pickup before destroying
+                if (spawnObject != null) {
+                    GameObject.Instantiate(spawnObject, transform.position, Quaternion.identity);
+                }
+
+                // destroy object
+                Destroy(gameObject);
+            }
+
+        }
     }
-  }
+
+    void OnTriggerStay2D(Collider2D collisionInfo) {
+        DestroysBreakables breaker = collisionInfo.gameObject.GetComponent<DestroysBreakables>();
+        if (breaker == null || (onlyDynamite && !breaker.isExplosion)) return;
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 || breaker.isExplosion) {
+            countdown = toughness;
+            alterDurability(breaker.durabilityImpact);
+        }
+    }
 
 }
