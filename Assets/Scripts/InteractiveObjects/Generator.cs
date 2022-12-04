6eae5cf6d6925395764e00fdc598e01268d4a185
Salty Fish <saltyfish@sjtu.edu.cn>
2022-12-04 01:44:13 -0500

    fix: generator trigerred by non-player

--- Assets/Scripts/InteractiveObjects/Generator.cs
+++ Assets/Scripts/InteractiveObjects/Generator.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    AudioSource chargeSFX;
-    [SerializeField] int generatorIndex = 0;
+  AudioSource chargeSFX;
+  [SerializeField] int generatorIndex = 0;
 
-    private void Start() {
-        chargeSFX = GetComponent<AudioSource>();
-    }
+  private void Start()
+  {
+    chargeSFX = GetComponent<AudioSource>();
+  }
 
-    void OnTriggerEnter2D(Collider2D collision) {
-        if (chargeSFX != null) {
-            chargeSFX.Play();
-        }
-        EventBus.Publish(new EventBatteryStatusChange { charging = true });
+  void OnTriggerEnter2D(Collider2D collisionInfo)
+  {
+    if (collisionInfo.gameObject == Gameplay.player)
+    {
+      if (chargeSFX != null)
+        chargeSFX.Play();
+      EventBus.Publish(new EventBatteryStatusChange { charging = true });
     }
+  }
 
-    void OnTriggerExit2D(Collider2D collision) {
-        EventBus.Publish(new EventBatteryStatusChange { charging = false });
-    }
+  void OnTriggerExit2D(Collider2D collisionInfo)
+  {
+    if (collisionInfo.gameObject == Gameplay.player)
+      EventBus.Publish(new EventBatteryStatusChange { charging = false });
+  }
 
 }
