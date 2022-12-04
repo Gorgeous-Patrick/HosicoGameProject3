b429086b5b1f42cf2f6b377f7250ed1b5f4bd8ca
henrlin24 <102765166+henrlin24@users.noreply.github.com>
2022-12-04 00:11:42 -0500

    Health UI update; added animation/shift

--- Assets/Scripts/Global/DisplaysHealth.cs
+++ Assets/Scripts/Global/DisplaysHealth.cs
@@ -7,9 +7,12 @@ public class DisplaysHealth : MonoBehaviour
 {
     private TextMeshProUGUI healthText;
     Subscription<EventShowHealthUI> UI_event_subscription;
+    Animator anim;
 
     void Start() {
         healthText = GetComponent<TextMeshProUGUI>();
+        anim = GetComponent<Animator>();
+
         EventBus.Subscribe<EventUpdateHealth>((e) => healthText.text = $"x {e.newHealth}");
         UI_event_subscription = EventBus.Subscribe<EventShowHealthUI>(_changeRed);
     }
@@ -17,7 +20,7 @@ public class DisplaysHealth : MonoBehaviour
     private void _changeRed(EventShowHealthUI e) {
         if (e.isRed) {
             if (healthText != null) {
-                healthText.color = Color.red;
+                anim.SetTrigger("red");
             }
         }
     }
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -336,7 +336,7 @@ public class PlayerControl : MonoBehaviour
         if (e.resetEntireLevel) {
             // fade in UI element
             EventBus.Publish(new EventShowHealthUI { isStart = true, isRed = true });
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(3.0f);
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             PlayerPrefs.SetString("currScene", SceneManager.GetActiveScene().name);
