2da3b2298bb4370953e1b95e10010c7c4a85f20f
Patrick Li <1543162420@qq.com>
2022-12-05 08:18:35 -0500

    move api to a new script

--- Assets/Scripts/Global/ScreenShakeManager.cs
+++ Assets/Scripts/Global/ScreenShakeManager.cs
@@ -26,6 +26,7 @@ public class ScreenShakeManager : MonoBehaviour
   void Start()
   {
     virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        StartCoroutine(test());
   }
 
   void StartShaking(float amplitude, float frequency)
@@ -41,4 +42,22 @@ public class ScreenShakeManager : MonoBehaviour
     noise.m_AmplitudeGain = 0;
     noise.m_FrequencyGain = 0;
   }
+    IEnumerator Shaking(float intensity, float frequency, float time)
+    {
+        StartShaking(intensity, frequency);
+        yield return new WaitForSeconds(time);
+        StopShaking();
+    }
+
+    public void ShakingCoroutine(float intensity = 3, float frequency = 5, float time = 0.5f)
+    {
+        StartCoroutine(Shaking(intensity, frequency, time));
+    }
+
+    IEnumerator test()
+    {
+        yield return new WaitForSeconds(5);
+        Debug.Log("Shaking");
+        ShakingCoroutine();
+    }
 }
