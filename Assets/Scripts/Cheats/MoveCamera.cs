ddce57fda2d6da9e266ee03671db3f0e71178b17
henrlin24 <102765166+henrlin24@users.noreply.github.com>
2022-12-06 18:17:19 -0500

    Update to PlayerControl.cs to not crash when battery canvas is inactive. Trailer-specific scripts.

--- Assets/Scripts/Cheats/MoveCamera.cs
+++ Assets/Scripts/Cheats/MoveCamera.cs
@@ -4,27 +4,22 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    GameObject MovableCamera;
     [SerializeField] float offsetValue = 1.0f;
-
-    private void Start() {
-        MovableCamera = GameObject.Find("Camera");
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow)) {
-            MovableCamera.transform.position = new Vector3(MovableCamera.transform.position.x, MovableCamera.transform.position.y + offsetValue, MovableCamera.transform.position.z);
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + offsetValue, this.gameObject.transform.position.z);
         }
         else if (Input.GetKey(KeyCode.DownArrow)) {
-            MovableCamera.transform.position = new Vector3(MovableCamera.transform.position.x, MovableCamera.transform.position.y - offsetValue, MovableCamera.transform.position.z);
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - offsetValue, this.gameObject.transform.position.z);
         }
         else if (Input.GetKey(KeyCode.LeftArrow)) {
-            MovableCamera.transform.position = new Vector3(MovableCamera.transform.position.x - offsetValue, MovableCamera.transform.position.y, MovableCamera.transform.position.z);
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x - offsetValue, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
         }
         else if (Input.GetKey(KeyCode.RightArrow)) {
-            MovableCamera.transform.position = new Vector3(MovableCamera.transform.position.x + offsetValue, MovableCamera.transform.position.y, MovableCamera.transform.position.z);
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x + offsetValue, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
         }
     }
 }
