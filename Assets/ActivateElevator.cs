459cb671ed88c126b30fc92010408ceb8abf6ea3
henrlin24 <102765166+henrlin24@users.noreply.github.com>
2022-12-09 18:08:40 -0500

    Final build

--- Assets/ActivateElevator.cs
+++ Assets/ActivateElevator.cs
@@ -7,36 +7,38 @@ using UnityEngine.UI;
 
 public class ActivateElevator : Interactive
 {
-  public UnityEvent disablePromptEvent;
+    public UnityEvent disablePromptEvent;
 
-  [SerializeField] string targetSceneName;
+    [SerializeField] string targetSceneName;
+    [SerializeField] GameObject Background;
 
-  bool activated = false;
+    bool activated = false;
 
-  protected override void Interact()
-  {
-    Gameplay.playerInput.Disable();
-    disablePromptEvent?.Invoke();
-    activated = true;
-    GameObject.Find("Background").GetComponent<Image>().color = Color.white;
-    StartCoroutine(TransitionCoroutine());
-  }
+    protected override void Interact() {
+        Gameplay.playerInput.Disable();
+        disablePromptEvent?.Invoke();
+        activated = true;
+    }
 
-  void Update()
-  {
-    if (activated)
-      transform.Translate(Vector2.up * 3f * Time.deltaTime);
-  }
+    void Update() {
+        if (activated) {
+            transform.Translate(Vector2.up * 3f * Time.deltaTime);
+            if (Background != null) {
+                Background.GetComponent<Image>().color = Color.white;
+            }
+            
+            StartCoroutine(TransitionCoroutine());
+        }
+    }
 
-  IEnumerator TransitionCoroutine()
-  {
-    // buy more time for elevator to go beyond screen edge
-    yield return new WaitForSeconds(1.5f);
+    IEnumerator TransitionCoroutine() {
+        // buy more time for elevator to go beyond screen edge
+        yield return new WaitForSeconds(1.5f);
 
-    EventBus.Publish<EventStartTransition>(new EventStartTransition { isExtendedStart = true });
-    yield return new WaitForSeconds(4.0f);
+        EventBus.Publish<EventStartTransition>(new EventStartTransition { isExtendedStart = true });
+        yield return new WaitForSeconds(4.0f);
 
-    // load target scene
-    SceneManager.LoadScene(targetSceneName);
-  }
+        // load target scene
+        SceneManager.LoadScene(targetSceneName);
+    }
 }
